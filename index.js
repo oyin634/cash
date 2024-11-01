89d43c95164107028c6889acf85b9dbad9d5de4a
oyin634 <debayoroodiyah2@gmail.com>
2024-11-01 11:42:07 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+let fruit = ["mango","pineapple","watermelon","colours"]
+let car = ["volvo","honda","bmw"]
+let names = ["john","jane","bob"]
+
+
+
+let fruitcars = fruit.concat(car,names)
+console.log(fruitcars)
+
+
+
+function backgroundchanger(){
+    let color = ["litghtblue"]
+    document.getElementById("body").style.backgroundColor= "lightblue"
+}
+function fontsize(){
+    document.getElementById("font").style.fontSize = "300px"
+}
+function textchanger(){
+    document.getElementById("inner").innerHTML="enjoy exploring"
+}
+function Alerto(){
+    alert("you just clicked the button")
+}
